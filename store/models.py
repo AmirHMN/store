038fe2d00464fbb0a904c5b8016fd98d80738e66
AmirHmn <amir.hosein6979@gmail.com>
2022-01-31 10:47:41 +0330

    create Customer model for store app

--- store/models.py
+++ store/models.py
@@ -7,3 +7,17 @@ class Products(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+
+
+class Customer(models.Model):
+    MEMBERSHIP_CHOICES = (
+        ('B', 'Bronze'),
+        ('S', 'Silver'),
+        ('G', 'Gold')
+    )
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=12)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES)
